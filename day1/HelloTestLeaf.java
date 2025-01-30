62d130105261b1c3be79d355fe0067a304202b5b
diva93 <129885739+diva93@users.noreply.github.com>
2025-01-30 14:29:35 +0530

    Add files via upload
    day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	
+	
+	public static void main(String[] args) { 
+		float browserVersion = 100.2f;
+		String browserName = "chrome";
+		boolean isVisible = true;
+		int releaseYear = 1993;
+		char browserLogo = 'c';
+		
+		System.out.println("Browser version: " +browserVersion);
+		System.out.println("Brwoser Name: " +browserName);
+		System.out.println("Brwoser visibility: " +isVisible);
+		System.out.println("Release Year: " +releaseYear);
+		System.out.println("brwoser Logo: " +browserLogo);
+
+		
+}
+}
